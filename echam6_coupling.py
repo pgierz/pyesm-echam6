f41b3923ac96baf927e88004971961ced9c66fc9
Paul Gierz <pgierz@awi.de>
2019-01-23 14:05:17 +0100

    echam to generic atmosphere works

--- echam6_coupling.py
+++ echam6_coupling.py
@@ -6,9 +6,14 @@ Allows for coupling between ``ECHAM6`` and a **generic ice sheet**
 
 # Standard Library Imports:
 import glob
+import logging
+import os
+import tempfile
 
 # This Library Imports:
-from echam.echam_simulation import EchamCompute
+from pyesm.echam6.echam6_simulation import Echam6Compute
+from pyesm.helpers import load_environmental_variable_1_0, ComponentFile, FileDict
+from pyesm.time_control import CouplingEsmCalendar
 
 # External Imports:
 import cdo
@@ -20,16 +25,28 @@ ACCELERATION_DUE_TO_GRAVITY = 9.81  # m/s**2
 # all the functionality:
 
 
-class EchamCouple_Ice(EchamCompute):
+class Echam6Couple_Ice(Echam6Compute):
     """ Contains functionality to cut out ice sheet forcing from ECHAM6 output """
-    def __init__(self):
-        # FIXME: real syntax for the next line...
-        super().__init__()
+    def __init__(self, **EchamComputeArgs):
+        # FIXME: This all belongs in a generalized class, not here...
+        super(Echam6Couple_Ice, self).__init__(**EchamComputeArgs)
+
+        try:
+            assert isinstance(self.calendar, CouplingEsmCalendar)
+        except AssertionError:
+            raise TypeError("You must supply a calendar with coupling functionality: CouplingEsmCalendar, and not %s" % type(self.calendar))
+
+        self.files["couple"] = FileDict()
+        self._register_directory("couple", use_Name="generic")
 
         self.__cleanup_list = []
-        self._cdo_stderr = open("EchamCouple_Ice_cdo_log", "w")
-        self._nco_stderr = open("EchamCouple_Ice_nco_log", "w")
+        self._cdo_stderr = open(self.couple_dir+"/EchamCouple_Ice_cdo_log", "w")
+        self._nco_stderr = open(self.couple_dir+"/EchamCouple_Ice_nco_log", "w")
         self.CDO = cdo.Cdo(logging=True, logFile=self._cdo_stderr)
+
+        # Get relevant environmental variables
+        self.ECHAM_TO_ISM_multiyear_mean = load_environmental_variable_1_0("ECHAM_TO_ISM_multiyear_mean")
+        self.ECHAM_TO_ISM_time_mean = load_environmental_variable_1_0("ECHAM_TO_ISM_time_mean")
 
     def send_ice(self):
         """ Sends a generic atmosphere field for an ice sheet model """
@@ -49,48 +66,91 @@ class EchamCouple_Ice(EchamCompute):
         """
         logging.info("\t\t Preparing echam6 file for processing in an ice sheet model...")
 
-        start_date = self.calendar.coupling_start_date
-        end_date = self.calendar.coupling_end_date
+        start_year = self.calendar.coupling_start_date[self.name].format('YYYY')
+        end_year = self.calendar.coupling_end_date[self.name].format('YYYY')
 
-        file_list = self._construct_input_list(start_date, end_date)
+        file_list = self._construct_input_list(start_year, end_year)
         files_with_selected_variables = self._select_relevant_variables(file_list)
 
         final_output = self._concatenate_files(files_with_selected_variables)
-        if ECHAM_TO_ISM_multiyear_mean:
+
+        if self.ECHAM_TO_ISM_multiyear_mean:
             final_output = self._multiyear_mean(final_output)
+        if self.ECHAM_TO_ISM_time_mean:
+            final_output = self._time_mean(final_output)
 
         self.files["couple"]["atmosphere_file_for_ice"] = ComponentFile(src=final_output,
-                                                                        dest=self._couple_dir+"/atmosphere_file_for_ice.nc")
+                                                                        dest=self.couple_dir+"/atmosphere_file_for_ice.nc")
         logging.info("\t\t ...done!")
 
     def _write_grid_description(self):
         """ Writes echam6 grid descrption to atmosphere.griddes """
         logging.info("\t\t Writing echam6 grid description to generic file atmosphere.griddes...")
         logging.info("\t\t *   generatic griddes")
-        ofile = self.CDO.griddes(input=self.files["couple"]["atmosphere_file_for_ice"].src)
-        self.files["couple"]["atmosphere_grid_description"] = ComponentFile(src=ofile, dest=self._couple_dir+"/atmosphere.griddes")
+        griddes = self.CDO.griddes(input=self.files["couple"]["atmosphere_file_for_ice"].src)
+        ofile = open(self.couple_dir+"/griddes_file", "w")
+        ofile.write("\n".join(griddes))
+        ofile.flush()
+        self.files["couple"]["atmosphere_grid_description"] = ComponentFile(src=ofile.name,
+                                                                            dest=self.couple_dir+"/atmosphere.griddes")
+        self.__cleanup_list.append(ofile.name)
         logging.info("\t\t ...done!")
 
     def _write_variable_description(self):
         """Writes variable descrptions to a file"""
         pass
 
-    def _construct_file_list(self, start_date, end_date):
+    def _construct_input_list(self, start_year, end_year):
         logging.info("\t\t *   constructing input list...")
-        file_list =  [glob.glob(self.outdata_dir+"/"+self.expid+"_echam6_echam_"+start_date+"-"+end_date+"*.nc")]
+        # FIXME: This depends on how the model is configured to output data.
+        # This **SHOULD** be defined somehow in one of the namelists...
+        file_list = []
+        for year in range(int(start_year), int(end_year)):
+                for month in range(1, 12):
+                        datestamp=str(year).zfill(4)+str(month).zfill(2)
+                        file_list.append(self.outdata_dir+"/"+self.expid+"_echam6_echam_"+datestamp+".grb")
         return file_list
 
     def _select_relevant_variables(self, file_list):
         logging.info("\t\t *   selecting relevant variables...")
         files_with_selected_variables = []
-        for this_file in file_list:
-            ofile = self.CDO.expr("orog=geosp/%s(ACCELERATION_DUE_TO_GRAVITY); aprt=aprl+aprc; temp2=temp2" % (ACCELERATION_DUE_TO_GRAVITY),
-                             input=this_file)
-            for lst in files_with_selected_variables, self.__cleanup_list:
-                lst.append(ofile)
+        with tempfile.NamedTemporaryFile() as instruction_file:
+            # Write instructions to a file:
+            instruction_file.write("orog=geosp/%(g)s; \n" % {"g": ACCELERATION_DUE_TO_GRAVITY})
+            instruction_file.write("aprt=aprl+aprc; \n")
+            instruction_file.write("temp2=temp2; \n")
+            instruction_file.write("bottom_sw_down=srads-sradsu; \n")
+
+            # Save the file, it will be deleted after the with statement closes
+            instruction_file.flush()
+            # Sorry, this has to be made by hand...
+            required_vars = ["geosp", "aprl", "aprc", "temp2", "srads", "sradsu"]
+
+            # Run the loop
+            for this_file in file_list:
+                vars_in_this_file = self.CDO.pardes(input=this_file, options="-t echam6")
+                # Use only the second entry, the first is the code number, the
+                # rest is long name
+                vars_in_this_file = [var.split()[1] for var in vars_in_this_file]
+                if set(required_vars).issubset(set(vars_in_this_file)):
+                    ofile = self.CDO.exprf(instruction_file.name,
+                                           input=this_file, options="-t echam6 -f nc")
+                    for lst in files_with_selected_variables, self.__cleanup_list:
+                        lst.append(ofile)
+                else:
+                    logging.warn("\t\t *   WARNING: Not all variables needed were present, skipping %s", this_file)
+                    logging.debug("These were needed: ", required_vars)
+                    logging.debug("These were available: ", vars_in_this_file)
         return files_with_selected_variables
 
     def _concatenate_files(self, file_list):
         logging.info("\t\t *   concatenating files...")
-        ofile = self.CDO.cat(input=file_list)
+        return self.CDO.cat(input=file_list, options="-f nc")
 
+    def _multiyear_mean(self, ifile):
+        logging.info("\t\t *   generating multi-year monthly mean...")
+        return self.CDO.ymonmean(input=ifile, options="-f nc")
+
+    def _time_mean(self, ifile):
+        logging.info("\t\t *   generating full time mean...")
+        return self.CDO.timmean(input=ifile, options="-f nc")
